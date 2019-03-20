d7c2651c2e67cce49d49ec1cf7e38d46fe8b9149
rob3rtroessler <robertroessler@g.harvard.edu>
2019-03-20 12:03:59 -0400

    client-server communication established via axios

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
-const express = require("express");
-const path = require('path');
-//const mysql = require('mysql');
-
-// Initialize Express
+const express = require('express');
+const util = require('util');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 const app = express();
+const port = 4000;
+
+// setting up path for static scripts
 app.use(express.static('static'));
 
+// setting up bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// render index.html
 app.get('/',function(req,res){
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/start.html');
+});
+
+// getting corpus data;
+app.post('/corpus', (request, response) => {
+    console.log(request.body)
 });
 
 
+// listen on ${port}
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
 
-let port = process.env.PORT;
-
-if (port == null || port == "") {
-  port = 8000;
-}
-app.listen(port);
-
-// log
-console.log("Running on localhost:8000");
+    console.log(`server is listening on ${port}`)
+});
